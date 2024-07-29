8f3f5ab7c312225b1d98584cc53651ce14e76a64
Savan Fami <savanfami1239@gmail.com>
2024-07-29 14:38:41 +0530

    fixed nodemailer bugs

---
+++ notification-service/src/infrastructure/kafka/consumer/index.ts
@@ -0,0 +1,5 @@
+import userCreatedconsumer from "./userCreatedconsumer";
+
+export {
+    userCreatedconsumer
+}
---
+++ notification-service/src/infrastructure/kafka/consumer/userCreatedconsumer.ts
@@ -0,0 +1,28 @@
+import { generateVerificationMail } from '../../mail/generateVerificationMail'
+
+// interface IUserCreatedData {
+//     email: string;
+//     otp: string;
+//   }
+
+
+export default async(
+    data:any
+    
+)=>{
+    try{
+
+        console.log('consumer working mail sending on process')
+        console.log(data,'dattaaaaaa froma auth')
+        // const parsedData:IUserCreatedData=JSON.parse(data)
+        const email=data.email
+        const otp=data.otp 
+        const username=data.username
+        if (!email ||!otp ) {
+            throw new Error('Missing email or OTP in the data');
+          }
+          await generateVerificationMail(email,otp,username);
+    }catch(error){
+        console.log(error)
+    }
+}
