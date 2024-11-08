4103766b42e61ed0467afe1f884c0c4d242dc824
Savan Fami <savanfami1239@gmail.com>
2024-11-08 14:29:47 +0530

    added redis

--- auth-service/src/infrastructure/database/redis/redisClient.ts
+++ auth-service/src/infrastructure/database/redis/redisClient.ts
@@ -1,7 +1,22 @@
-import { Redis } from "ioredis"
+// import { Redis } from "ioredis"
 
-export const Client=new Redis()
+// export const Client=new Redis()
 
-Client.on("connect",()=>{
-    console.log('connected to redis')
-})
+// Client.on("connect",()=>{
+//     console.log('connected to redis')
+// })
+
+import { Redis } from "ioredis";
+
+const redisURL = "rediss://red-csmt12qj1k6c73do3fr0:EZfc4ESbYfrnHYh3BI9wrWLLYmbQ9Hvu@oregon-redis.render.com:6379";
+
+
+export const Client = new Redis(redisURL);
+
+Client.on("connect", () => {
+  console.log("Connected to Redis");
+});
+
+Client.on("error", (err) => {
+  console.error("Redis connection error:", err);
+});
